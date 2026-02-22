b0f5cf17d947ab988bead9cd249a367c2566a062
Hussain Khan <khanhussainpk4564@gmail.com>
2026-02-22 11:37:45 -0500

    added gitignore

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("This is a namaz and ftar time reminding bot.")
+import requests 
+import json 
+
+with open ("config.json", "r") as f:
+    config = json.load(f)
+    lat = config["lat"]
+    lon = config ["lon"]
+
+
+
+
+
+print("This is a namaz and ftar time reminding bot.")   
