776f33489d065e4544e2772f0d5c6123dbfe2c23
Hussain Khan <khanhussainpk4564@gmail.com>
2026-02-25 16:59:48 -0500

    Adding timings

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import requests 
-import json 
+import json
+import time  
+from datetime import datetime
+from prayertimes import prayer_times
+
+
 
 with open ("config.json", "r") as f:
     config = json.load(f)
     lat = config["lat"]
     lon = config ["lon"]
 
+print("This is a namaz and ftar time reminding bot.") 
 
 
+print(prayer_times)
+while True:
+    today = datetime.now().date()
+    current_time = datetime.now().strftime("%I:%M %p")
 
+    print(f"the date is {today} and hour is {current_time}")
+    
+    for prayer_name, prayer_time in prayer_times.items():
+        if current_time == prayer_time:
+            print(f"it is time for {prayer_name}")
 
-print("This is a namaz and ftar time reminding bot.")   
+    time.sleep(60)
---
+++ prayertimes.py
@@ -0,0 +1,7 @@
+prayer_times ={
+    "fajr" : "05:21 AM",
+    "duhr" : "12:09 PM",
+    "asar" : "03:14 PM",
+    "maghribh" : "04:29 PM",
+    "isha" : "06:58 PM"
+}
