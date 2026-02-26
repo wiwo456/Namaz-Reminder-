06486937e5e41df15021b5d66c8be251c98315fc
Hussain Khan <khanhussainpk4564@gmail.com>
2026-02-26 12:04:31 -0500

    added iftar logic

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import json
 import time  
 from datetime import datetime
+from datetime import timedelta
 from prayertimes import prayer_times
-
 
 
 with open ("config.json", "r") as f:
@@ -12,15 +12,26 @@ with open ("config.json", "r") as f:
     lon = config ["lon"]
 
 print("This is a namaz and ftar time reminding bot.") 
-
-
 print(prayer_times)
+
+ramadan_end = datetime(2026, 3, 19).date()
+
 while True:
     today = datetime.now().date()
     current_time = datetime.now().strftime("%I:%M %p")
 
     print(f"the date is {today} and hour is {current_time}")
-    
+
+    if today <= ramadan_end:
+        maghribh_str = prayer_times["maghribh"]
+        maghribh_conv = datetime.strptime(maghribh_str, "%I:%M %p") 
+
+        iftar_warning = maghribh_conv - timedelta(minutes=5)
+        iftar_warning_str = iftar_warning.strftime("%I:%M %p")
+
+        if current_time == iftar_warning_str:
+            print("Iftar is 5 minutes...")
+
     for prayer_name, prayer_time in prayer_times.items():
         if current_time == prayer_time:
             print(f"it is time for {prayer_name}")
--- prayertimes.py
+++ prayertimes.py
@@ -2,6 +2,6 @@ prayer_times ={
     "fajr" : "05:21 AM",
     "duhr" : "12:09 PM",
     "asar" : "03:14 PM",
-    "maghribh" : "04:29 PM",
+    "maghribh" : "05:43 PM",
     "isha" : "06:58 PM"
 }
